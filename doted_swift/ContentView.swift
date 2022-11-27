72e34445599f7aa67fb93d4d010314dc94a8ce2b
Ricardo da Silva Ogliari <rogliariping@gmail.com>
2022-11-26 21:30:46 -0300

    Initial Commit

---
+++ doted_swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  doted_swift
+//
+//  Created by Ricardo on 26/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ doted_swift/doted_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  doted_swiftApp.swift
+//  doted_swift
+//
+//  Created by Ricardo on 26/11/22.
+//
+
+import SwiftUI
+
+@main
+struct doted_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
